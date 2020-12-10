25f593435c04cc0f9628166562b7d79fbc0cafb4
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-12-10 13:17:31 +0530

    Changed getPolicyByUser for current schema

--- src/service/policyService.js
+++ src/service/policyService.js
@@ -1,19 +1,22 @@
 const { User, Policy } = require("../models");
 
 function getPolicyInfoByUsername(userName) {
-  return User.findOne({ first_name: userName }).then((user) => {
-    return Policy.find({ user: user._id })
-      .select("-__v -_id -user")
-      .populate("agent", "-__v -_id")
-      .populate("lob", "-__v -_id")
-      .populate("carrier", "-__v -_id");
-  });
+  return User.findOne({ first_name: userName })
+    .select("-__v -_id")
+    .populate({
+      path: "policy",
+      select: "-__v -_id -user",
+      populate: [
+        { path: "agent", select: "-__v -_id" },
+        { path: "lob", select: "-__v -_id" },
+        { path: "carrier", select: "-__v -_id" },
+      ],
+    });
 }
-
 function getAggregatedPolicy() {
   return Policy.find({})
     .select("-__v -_id")
-    .populate("user", "-__v -_id")
+    .populate("user", "-__v -_id -policy")
     .populate("agent", "-__v -_id")
     .populate("lob", "-__v -_id")
     .populate("carrier", "-__v -_id");
